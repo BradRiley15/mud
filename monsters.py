aace6a1ce37b7c726bfb23302b336523ccf6c12b
AHecky3 <ahecky@stcharlessd.org>
2014-10-14 08:27:22 -0500

    Update monsters.py

--- monsters.py
+++ monsters.py
@@ -1,1 +1,49 @@
 #This file will be used to store monster stats and if they appear etc.
+
+"""
+Goblins | Level 1
+
+  Health - Range from 7-13
+  Armor - Range from 5-17
+  Strength - Range from 3 - 19
+    - if strength > 13 = + 1 Damage to (Axe, Short Sword)
+    - if strength > 15 = + 2 Damage to (Axe, Short Sword)
+    - if strength > 17 = + 3 Damage to (Axe, Short Sword)
+  Dexterity - Range from 5-15
+    - if dexterity > 12 = + 1 Damage to (Bow, Dagger)
+    - if dexterity > 12 = + 1 Damage to (Bow, Dagger)
+  
+  Possible Weapons
+    - Axe
+      1-8 Damage
+    - Bow
+      1-6 Damage
+    - Dagger
+      1-4 Damage
+    - Short Sword
+      1-6 Damage
+"""
+"""
+Goblins | Level 2
+
+  Health - Range from 10-16
+  Armor - Range from 8-17
+  Strength - Range from 7-20
+    - if strength > 15 = + 1 Damage to (Axe, Short Sword)
+    - if strength > 17 = + 2 Damage to (Axe, Short Sword)
+    - if strength > 19 = + 3 Damage to (Axe, Short Sword)
+  Dexterity - Range from 5-15
+    - if dexterity > 12 = + 1 Damage to (Bow, Dagger)
+    - if dexterity > 12 = + 1 Damage to (Bow, Dagger)
+  
+  Possible Weapons
+    - Axe
+      1-8 Damage
+    - Bow
+      1-6 Damage
+    - Dagger
+      1-4 Damage
+    - Short Sword
+      1-6 Damage
+"""
+
