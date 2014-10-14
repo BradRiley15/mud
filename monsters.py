6f91a7e39bc820c724e9820ef45db6936e46c1c3
AHecky3 <ahecky@stcharlessd.org>
2014-10-14 10:03:35 -0500

    Update monsters.py

--- monsters.py
+++ monsters.py
@@ -1,49 +1,33 @@
 #This file will be used to store monster stats and if they appear etc.
 
-"""
-Goblins | Level 1
+#Setting Armor Ranges
+noArmor = random.randint(3,5)
+leatherArmor = random.randint(6,8)
+chainArmor = random.randint(9,11)
+plateArmor = random.randint(12,14)
+PossibleArmor = [noArmor,leatherArmor,chainArmor,plateArmor]
 
-  Health - Range from 7-13
-  Armor - Range from 5-17
-  Strength - Range from 3 - 19
-    - if strength > 13 = + 1 Damage to (Axe, Short Sword)
-    - if strength > 15 = + 2 Damage to (Axe, Short Sword)
-    - if strength > 17 = + 3 Damage to (Axe, Short Sword)
-  Dexterity - Range from 5-15
-    - if dexterity > 12 = + 1 Damage to (Bow, Dagger)
-    - if dexterity > 12 = + 1 Damage to (Bow, Dagger)
+#Goblin | Level 1
+import random
+import "mud/ArmorStats.py"
+
+#Setting Goblins Health, Speed, Stamina, Dexterity, Strength
+goblinHealth = random.randint(7,14)
+goblinSpeed = random.randint(3,7)
+goblinStamina = 20
+goblinDexterity = random.randint(3,18)
+goblinStrength = random.randint(3,19)
+
+#Setting Goblins Armor
+goblinArmor = random.choice(PossibleArmor)
+#Possible Armor Buffs
+if goblinDexterity > 13 :
+  goblinArmor = goblinArmor + 1
+if goblinDexterity > 15 :
+  goblinArmor = goblinArmor + 2
+if goblinDexterity > 17 :
+  goblinArmor = goblinArmor + 3
   
-  Possible Weapons
-    - Axe
-      1-8 Damage
-    - Bow
-      1-6 Damage
-    - Dagger
-      1-4 Damage
-    - Short Sword
-      1-6 Damage
-"""
-"""
-Goblins | Level 2
-
-  Health - Range from 10-16
-  Armor - Range from 8-17
-  Strength - Range from 7-20
-    - if strength > 15 = + 1 Damage to (Axe, Short Sword)
-    - if strength > 17 = + 2 Damage to (Axe, Short Sword)
-    - if strength > 19 = + 3 Damage to (Axe, Short Sword)
-  Dexterity - Range from 5-15
-    - if dexterity > 12 = + 1 Damage to (Bow, Dagger)
-    - if dexterity > 12 = + 1 Damage to (Bow, Dagger)
-  
-  Possible Weapons
-    - Axe
-      1-8 Damage
-    - Bow
-      1-6 Damage
-    - Dagger
-      1-4 Damage
-    - Short Sword
-      1-6 Damage
-"""
-
+#Possible Hit Buff
+if goblinDexterity > 13 :
+  goblinHitbuff = goblinHitchance + 
