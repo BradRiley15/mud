1115fc3ff990f336787dbdb22bace5ef70435ca7
AHecky3 <ahecky@stcharlessd.org>
2014-10-15 09:06:41 -0500

    Variables
    Set variables at the top of the file, so they stay the same throughout the entire code

--- mud-game-remake.py
+++ mud-game-remake.py
@@ -1,1 +1,8 @@
+ #Attempt at making a MUD Game. By: Bradly Riley and Andrew Hecky
+ """
+ So first things first, we should set variable names, so everyone knows what the variables are, and so they stay
+ the same throughout the enitre code.
+ 
+ Player Variables:
+ -----------------
  
