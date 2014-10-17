00c474f2294a07f30eb9fa9fa0e72a2aa31d10f5
AHecky3 <ahecky@stcharlessd.org>
2014-10-17 10:03:37 -0500

    Update combat.py

--- combat.py
+++ combat.py
@@ -1,5 +1,8 @@
 #This is where combat functions will be
 
+#Author: Andrew Hecky
+#Date First Started: 10/14/2014
+#Date Last Edited : 10/17/2014
 
 def combatEncounter():
   #Setting Function Variables
@@ -14,9 +17,7 @@ def combatEncounter():
   valid_CombatTurn = False
   
   #Import Global Variables
-  global outOfCombat
-  global playerTurn
-  global enemy1Turn
+  global 
   
   #Setting Who Has First Turn
   combatTurn = outOfCombat
@@ -25,74 +26,96 @@ def combatEncounter():
   if playerSpeed < enemySpeed :
     combatTurn = enemy1Turn
 
-  #Actual Combat
-  while playerHealth > 0 and enemyHealth > 0 :
-  valid_CombatTurn = empty
+#Actual Combat
+
+#Loop 1 : playerHealth, enemyHealth, combatTurn
+  while playerHealth > 0 and enemyHealth > 0 and (combatTurn != outOfCombat) :
+  
+                        #While Players Turn
+#Loop 2 : combatTurn 
     while combatTurn == playerTurn :
-      #Player Chooses What To Do
-      print("What do you want to do? ")
-      print("You can (A)ttack, (R)un, (T)alk, (S)uicide, or do (N)othing. ")
-      playerCombatTurn = input("Choice: ")[0].upper()
-      #Deciding if Player Choice Is Valid
+  #Setting Choice To False to Run Loop
+      valid_CombatTurn = False
+#Loop 3 : valid_CombatTurn
       while not valid_CombatTurn :
-    #If Player Chooses To Attak
+  
+  #Player Chooses What To Do
+        print("What do you want to do? ")
+        print("You can (A)ttack, (R)un, (T)alk, (S)uicide, or do (N)othing. ")
+        playerCombatTurn = input("Choice: ")[0].upper()
+  
+  #If Player Chooses To Attak
         if playerCombatTurn == "A" :
           valid_CombatTurn = True
 
-    #If Player Chhoses To Run
+  #If Player Chooses To Run (Complete : Needs Tested)
         elif playerCombatTurn == "R" :
           valid_CombatTurn = True
           print("You turn and attempt to run! ")
           escapeChance = random.randit(1,20)
+      #If Player Escapes Successfully
           if escapeChance > 13 :
             print("You escape successfully! ")
-          
+            combatTurn = outOfCombat
+            playerCombatTurn = empty
+      #If Player Can't Escape
+          elif escapeChange <= 13
+            print("Oh no! You cannot escape! ")
+            cobatTurn = enemyTurn
+            playerCombatTurn = empty
   
-    #If Player Chooses To Talk
+  #If Player Chooses To Talk
         elif playerCombatTurn == "T" :
           valid_CombatTurn = True
   
-    #If Player Chooses To Commit Suicide
+  #If Player Chooses To Commit Suicide (Complete : Needs Tested)
         elif playerCombatTurn == "S" :
-          valid_CombatTurn = True
+#Loop 4 : playerCombatTurn is "S"
           while playerCombatTurn == "S" :
             print("\nYou draw your sword, holding it with the point of it's blade pressed against your chest. ")
+      #Confirming if player really wants to commit suicide
             player_Suicide = input("Are you sure? (Y/N): ")[0].upper()
-        #Confirming if player really wants to commit suicide
+          #Player Wants To Commit Suicide
             if player_Suicide == "Y" :
               print(\n"You take your readied sword and plunge it deep into your chest, killing you almost instantly. ")
               playerHealth = 0
-              valid_CombatTurn = False
+              valid_CombatTurn = True
               playerCombatTurn = empty
               player_Suicide = empty
+              combatTurn = outOfCombat
+          #Player Doesn't Want To Commit Suicide
             elif player_Suicide == "N" :
               print("\nYou lower your sword, deciding not to kill yourself. ")
               valid_CombatTurn = False
               playerCombatTurn = empty
               player_Suicide = empty
+          #If Player Chooses Else
             elif player_Suicide !="Y" or player_Suicide !="N" : #or player_Suicide !="--"
               print("\nThat is not a valid option. ")
               player_Suicide = input("Commit Suicide? (Y/N): ")[0].upper()
-            print(" ")
-    #If Player Choosees To Do Nothing      
+              
+  #If Player Choosees To Do Nothing (Complete : Needs Testes)
         elif playerCombatTurn == "N" :
           valid_CombatTurn = True
-          while playerCombatTurn == "N" :
-            print("Not moving a muscle, you stand still and do nothing. ")
-            combatTurn = enemyTurn
-            playerCombatTurn = empty
-     #If Player Chooses Invalid Option
+          print("Not moving a muscle, you stand still and do nothing. ")
+          combatTurn = enemyTurn
+          playerCombatTurn = empty
+  
+  #If Player Chooses Invalid Option (Complete : Needs Tested)
         else :
           valid_CombatTurn = False
           print("\nYou cannot do that!")
-    #Plater Re-Selects What They Want To Do 
-        if playerHealth > 0 :
+
+""" 
+***This block is currently removed, code I added earier in the loop should make this block obsolete.***
+#Player Re-Selects What They Want To Do 
+        if playerHealth > 0 and not valid_CombatTurn :
           print("What do you want to do? ")
           print("You can (A)ttack, (R)un, (T)alk, (S)uicide, or do (N)othing. ")
           playerCombatTurn = input("Choice: ")[0].upper()
+"""
 
 
-
-#If Player Dies
+  #If Player Dies
 while playerHealth <= 0 :
   print("Game Over...")
